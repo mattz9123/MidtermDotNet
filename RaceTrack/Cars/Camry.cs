3e4e46c861906d0a3bb65f385343490f3ae4b619
Matthew Zwerlein <mzwerlein@my.wctc.edu>
2022-03-07 19:04:41 -0600

    first commit

---
+++ RaceTrack/Cars/Camry.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTrack.RaceTrack.Cars
+{
+    public class Camry : RaceCar
+    {
+        public Camry()
+        {
+            Name = "Camry";
+            TopSpeed = 120;
+        }
+
+        public override void StartEngine()
+        {
+            Console.WriteLine($"The {Name} engine sounds terrible!");
+        }
+
+        public override void Brake()
+        {
+            Console.WriteLine($"The {Name} slows down!");
+            base.Brake();
+        }
+    }
+}
---
+++ RaceTrack/Drivers/Rob.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RaceTrack.RaceTrack.Cars;
+
+namespace RaceTrack.RaceTrack.Drivers
+{
+    public class Rob : Driver
+    {
+        public Rob(RaceCar car) : base(car)
+        {
+            Name = " Rob";
+            SkillLevel = 5;
+        }
+
+        public override void Drive()
+        {
+            Car.Accelerate(SkillLevel);
+        }
+
+        
+    }
+}
